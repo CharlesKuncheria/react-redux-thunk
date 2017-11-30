5a190311b101fb267166355fa7c8241ee3f3839e
Charles Kuncheria <chkunche@microsoft.com>
2017-11-30 12:58:03 +0530

    Added SW

--- app/index.jsx
+++ app/index.jsx
@@ -13,3 +13,16 @@ ReactDOM.render(
         <App/>
     </Provider>,
     document.getElementById('app'));
+
+// register sw
+if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using a more restrictive scope.
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('Service worker registration succeeded:', registration);
+    }).catch(function(error) {
+      console.log('Service worker registration failed:', error);
+    });
+  } else {
+    console.log('Service workers are not supported.');
+  }
