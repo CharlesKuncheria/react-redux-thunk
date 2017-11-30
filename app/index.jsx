4967390b90eeea1018e172ebcd8ae6334ad5567f
Charles Kuncheria <chkunche@microsoft.com>
2017-11-30 13:11:24 +0530

    capturing the basic fetch requests.

--- app/index.jsx
+++ app/index.jsx
@@ -19,7 +19,7 @@ if ('serviceWorker' in navigator) {
     // Register a service worker hosted at the root of the
     // site using a more restrictive scope.
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      console.log('Service worker registration succeeded:', registration);
+      //console.log('Service worker registration succeeded:', registration);
     }).catch(function(error) {
       console.log('Service worker registration failed:', error);
     });
--- build/sw.js
+++ build/sw.js
@@ -1,1 +1,6 @@
-console.log('hello from sw')
+self.addEventListener('fetch', function(event) { 
+    console.log(event.request);
+    if(event.request.url.endsWith('foo')) {
+        event.respondWith(new Response('bar'));
+    }
+})
